d267d8f06f129894293227c33fb0f68836c6c39b
Christian Rockwood <rockwood@Christians-MBP.lan>
2022-08-08 17:48:04 -0400

    build database calls

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,13 +8,28 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+//returns an array of each row of data
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
 // Default response for any other request (Not Found)--has to be last
 app.use((req, res) => {
     res.status(404).end();
   });
-  
-
-
 
 
 //this will start express.js server on port 3001
