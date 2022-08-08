fb1e9d5438e3c166123b0431ed2d8d0800c449d0
Christian Rockwood <rockwood@Christians-MBP.lan>
2022-08-08 17:23:57 -0400

    Test the Express.js Connection

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default response for any other request (Not Found)--has to be last
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+
+
+
+
+//this will start express.js server on port 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
