f956979d9ebb3dc6170dc2757fed4c2aef482055
rockwoodc <rockwoodce@gmail.com>
2022-08-10 14:50:33 -0400

    added voter table'

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,8 @@
+DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS voters;
+
 
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -26,4 +28,16 @@ CREATE TABLE voters (
   email VARCHAR(50) NOT NULL,
   -- will return the time of the server not the client time
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE votes (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  voter_id INTEGER NOT NULL,
+  candidate_id INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  -- signifies that the values inserted into the voter_id field must be unique
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  -- delete cascade ensures you are deleting the reference key & will also delete the entire row from this table
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
